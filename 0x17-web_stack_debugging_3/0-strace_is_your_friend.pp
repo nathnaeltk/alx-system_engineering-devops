8df79d0aadb8bdbbbe12205a84b36483f01e3c92
Nathnael Teketel <hellonathnael@gmail.com>
2022-11-02 22:04:25 +0300

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,11 +1,5 @@
-# fix file extension typo
-exec { 'replace phpp with php':
-  command  => 'sed -ie \'s/class-wp-locale.phpp/class-wp-locale.php/\' /var/www/html/wp-settings.php',
-  provider => shell
-}
-
-#restart apache2
-exec { 'restart apache2 service':
-  command  => 'service apache2 restart',
-  provider => shell
+# changes a mistyped line in wordpress apache configuration
+exec { 'wordpress hell':
+  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+  path    => '/bin',
 }
