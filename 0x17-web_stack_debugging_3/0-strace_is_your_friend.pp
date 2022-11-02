ba849653fdaef5546a0178b8b1fad90230dede71
Nathnael Teketel <hellonathnael@gmail.com>
2022-11-02 22:00:58 +0300

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,5 @@
-#Using strace to find out why Apache is returning a 500 error
-
-file { '/var/www/html/wp-includes/class-wp-locale.phpp':
-  ensure => file,
-  source => '/var/www/html/wp-includes/class-wp-locale.php',
-  mode   => '0664'
+# Corrects phpp extension for php
+exec {'phppFix':
+    path    => '/bin',
+    command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php"
 }
