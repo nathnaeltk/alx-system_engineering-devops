bbc61e66949ff500cf5a73f8d71cc9bd2dedf914
Nathnael Teketel <hellonathnael@gmail.com>
2022-11-02 22:01:39 +0300

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
-# Corrects phpp extension for php
-exec {'phppFix':
-    path    => '/bin',
-    command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php"
+# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
