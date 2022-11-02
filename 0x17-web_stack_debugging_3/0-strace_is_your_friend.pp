2c962e8885782b6729e86896a751429c59954b2f
Nathnael Teketel <hellonathnael@gmail.com>
2022-11-02 21:58:55 +0300

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,7 @@
-# Fix a typo within wp-settings.php
-exec { 'fix-wordpress':
-  command => '/bin/sed -i \'s/.phpp/.php/\' /var/www/html/wp-settings.php',
+#Using strace to find out why Apache is returning a 500 error
+
+file { '/var/www/html/wp-includes/class-wp-locale.phpp':
+  ensure => file,
+  source => '/var/www/html/wp-includes/class-wp-locale.php',
+  mode   => '0664'
 }
